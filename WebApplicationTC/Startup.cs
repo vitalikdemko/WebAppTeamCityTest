f823d52435c55786f22d7bdb6f2addc7f3aed0c5
vdemk0 <vdemk@exoft.net>
2019-01-09 19:28:17 +0200

    initial commit

--- WebApplicationTC/Startup.cs
+++ WebApplicationTC/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApplicationTC
 {
-    public class Startup
+    public class Startup 
     {
         public Startup(IConfiguration configuration)
         {
